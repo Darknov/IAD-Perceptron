43338e12f9a9f17f2d8d9ff5997171800508a3ff
Gnlknmd <dominik.lange@outlook.com>
2017-04-17 21:35:04 +0200

    NeuralNetwork::setBias()

--- Main.cpp
+++ Main.cpp
@@ -7,9 +7,7 @@ using namespace std;
 
 int main(int argc)
 {
-	
 
-	
 	
 	
 	cin.ignore();
--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -5,3 +5,9 @@
 NeuralNetwork::NeuralNetwork()
 {
 }
+
+void NeuralNetwork::setBias(bool exists)
+{
+	for (NeuralLayer l : layers)
+		l.setBiasPermissions(exists);
+}
